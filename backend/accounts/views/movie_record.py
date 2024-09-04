83c779e3a9368211b48b007e0cc8200fba7bae06
James Bang <jbanghw@gmail.com>
2024-09-04 13:39:15 -0400

    custom pagination for user's movie list

--- backend/accounts/views/movie_record.py
+++ backend/accounts/views/movie_record.py
@@ -1,36 +1,35 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import LimitOffsetPagination
 from accounts.models import MovieRecord
 from accounts.serializers import MovieRecordSerializer
 
+class CustomPagination(LimitOffsetPagination):
+    default_limit = 3
 
 class MovieRecordAPIView(ListAPIView):
     serializer_class = MovieRecordSerializer
     permission_classes = [IsAuthenticated]
-    pagination_class = None
+    pagination_class = CustomPagination
 
     def get_queryset(self):
         try:
             title_filter = self.request.query_params.get('title_filter')
             watched_filter = self.request.query_params.get('watched')
             date_order = self.request.query_params.get('date_order')
-            limit = int(self.request.query_params.get('limit'))
-            offset = int(self.request.query_params.get('offset'))
 
-
-            result = MovieRecord.objects.filter(user=self.request.user)
-
-            result = result.filter(title__contains=title_filter)
+            movie_records = MovieRecord.objects.filter(user=self.request.user)
+            movie_records = movie_records.filter(title__contains=title_filter)
             if watched_filter == '-1':
-                result = result.filter(watched=False)
+                movie_records = movie_records.filter(watched=False)
             elif watched_filter == '1':
-                result = result.filter(watched=True)
+                movie_records = movie_records.filter(watched=True)
             if date_order == '1':
-                result = result.order_by('-added_date')
+                movie_records = movie_records.order_by('-added_date')
             elif date_order == '0':
-                result = result.order_by('added_date')
+                movie_records = movie_records.order_by('added_date')
             
-            return result[offset:offset + limit]
+            return movie_records
 
         except:
             return []
