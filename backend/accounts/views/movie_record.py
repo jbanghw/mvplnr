c9b07b74779ace8d0e2c06a68bda7e67d2a68f1d
James Bang <jbanghw@gmail.com>
2024-09-04 10:11:52 -0400

    basic profile page with user's movie list

--- backend/accounts/views/movie_record.py
+++ backend/accounts/views/movie_record.py
@@ -7,20 +7,30 @@ from accounts.serializers import MovieRecordSerializer
 class MovieRecordAPIView(ListAPIView):
     serializer_class = MovieRecordSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = None
 
     def get_queryset(self):
-        title_filter = self.request.query_params.get('title_filter')
-        watched_filter = self.request.query_params.get('watched')
-        date_order = self.request.query_params.get('date_order')
+        try:
+            title_filter = self.request.query_params.get('title_filter')
+            watched_filter = self.request.query_params.get('watched')
+            date_order = self.request.query_params.get('date_order')
+            limit = int(self.request.query_params.get('limit'))
+            offset = int(self.request.query_params.get('offset'))
 
-        result = MovieRecord.objects.filter(user=self.request.user)
 
-        result = result.filter(title__contains=title_filter)
-        if watched_filter == '1':
-            result = result.filter(watched=True)
-        elif watched_filter == '0':
-            result = result.filter(watched=False)
-        print(result)
-        if date_order == '1':
-            return result.order_by('-added_date')
-        return result.order_by('added_date')
+            result = MovieRecord.objects.filter(user=self.request.user)
+
+            result = result.filter(title__contains=title_filter)
+            if watched_filter == '-1':
+                result = result.filter(watched=False)
+            elif watched_filter == '1':
+                result = result.filter(watched=True)
+            if date_order == '1':
+                result = result.order_by('-added_date')
+            elif date_order == '0':
+                result = result.order_by('added_date')
+            
+            return result[offset:offset + limit]
+
+        except:
+            return []
--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,5 +1,58 @@
+import { useContext, useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import AuthContext from "../contexts/AuthContext"
+import UserMovie from "../interfaces/UserMovie"
+
 const ProfilePage = () => {
-  return <h1>Profile Page</h1>
+  const { loggedIn } = useContext(AuthContext)
+  const navigate = useNavigate()
+  const moviesPerPage = 10
+
+  const [movies, setMovies] = useState<UserMovie[]>([])
+  const [pageCount, setPageCount] = useState(0)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [titleFilter, setTitleFilter] = useState('')
+  const [watchedFilter, setWatchedFilter] = useState(0)  // -1 for unwatched, 0 for both, 1 for watched
+  const [dateOrder, setDateOrder] = useState(1)  // 1 for desc, 0 for asc
+
+  useEffect(() => {
+    if (loggedIn === null) {
+      navigate('/login')
+    }
+
+    const fetchMovies = async () => {
+      const url = `/api/accounts/movies/?title_filter=${titleFilter}&watched=${watchedFilter}&limit=${moviesPerPage}&offset=${moviesPerPage * (currentPage - 1)}&date_order=${dateOrder}`
+      try {
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${loggedIn}`
+          }
+        })
+        const data = await response.json()
+        setPageCount(Math.ceil(data.count / moviesPerPage))
+        setMovies(data)
+      } catch (error) {
+        console.log('Failed to fetch my movies.', error)
+      }
+    }
+    fetchMovies()
+  }, [currentPage, titleFilter, watchedFilter, dateOrder])
+
+  return (
+    <div>
+      {
+        movies?.map((movie) => {
+          return <div key={movie.movie_id}>
+            {movie.title}
+            {movie.movie_id}
+            {movie.added_date}
+            {movie.watched}
+          </div>
+        })
+      }
+    </div>
+  )
 }
 
 export default ProfilePage
